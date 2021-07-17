5902f3b040b0277057dbe125d694e5a4e4946c00
saulotarsobc <saulotarsobc@gmail.com>
2021-07-17 15:34:16 -0300

    ok

--- opaSuite.py
+++ opaSuite.py
@@ -1,20 +1,90 @@
-import sys
 import pymongo
 from bson.json_util import dumps
 from bson import ObjectId
 
-params = sys.argv
-item = params[1]
-mydb = pymongo.MongoClient("mongodb://conectchat.com.br:27017/")["suite"]
+mydb = pymongo.MongoClient("mongodb://127.0.0.1:27017/")["suite"]
 
 # EXEMPLO
-""" if item == "getAllUsuarios":
-    cursor = (mydb["usuarios"].find())
-    list_cur = list(cursor)
-    json_data = dumps(list_cur, indent = 3)
-    print(json_data) """
+cursor = (mydb["usuarios"].find())
+list_cur = list(cursor)
+json_data = dumps(list_cur, indent = 3)
+print(json_data)
 
-if item == "getAllUsuarios":
+teste = {
+  "firstName": "John",
+  "lastName" : "doe",
+  "age"      : 26,
+  "address"  : {
+    "streetAddress": "naist street",
+    "city"         : "Nara",
+    "postalCode"   : "630-0192"
+  },
+  "phoneNumbers": [
+    {
+      "type"  : "iPhone",
+      "number": "0123-4567-8888"
+    },
+    {
+      "type"  : "home",
+      "number": "0123-4567-8910"
+    }
+  ]
+}
+
+data = {}
+
+data["teste"] = {
+  "firstName": "John",
+  "lastName" : "doe",
+  "age"      : 26,
+  "address"  : {
+    "streetAddress": "naist street",
+    "city"         : "Nara",
+    "postalCode"   : "630-0192"
+  },
+  "phoneNumbers": [
+    {
+      "type"  : "iPhone",
+      "number": "0123-4567-8888"
+    },
+    {
+      "type"  : "home",
+      "number": "0123-4567-8910"
+    }
+  ]
+}
+
+data["teste2"] = {
+  "firstName": "John",
+  "lastName" : "doe",
+  "age"      : 26,
+  "address"  : {
+    "streetAddress": "naist street",
+    "city"         : "Nara",
+    "postalCode"   : "630-0192"
+  },
+  "phoneNumbers": [
+    {
+      "type"  : "iPhone",
+      "number": "0123-4567-8888"
+    },
+    {
+      "type"  : "home",
+      "number": "0123-4567-8910"
+    }
+  ]
+}
+
+
+
+
+print(data)
+
+
+
+
+
+""" if item == "getAllUsuarios":
     cursor = (mydb["usuarios"].find())
     list_cur = list(cursor)
     json_data = dumps(list_cur)
@@ -60,4 +130,4 @@ if item == "setor":
     print(mydb["atendimentos"].count_documents({"status": params[2],"setor": ObjectId(params[3])}))
 
 if item == "finalizadoHoje":
-    print(mydb.atendimentos.count_documents({"status":"F","id_atendente":ObjectId(params[2])}))
+    print(mydb.atendimentos.count_documents({"status":"F","id_atendente":ObjectId(params[2])})) """
