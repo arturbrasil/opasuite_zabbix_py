97fc66c4e039da0dd8fb30b4ce6e39034bc9475d
saulotarsobc <saulotarsobc@gmail.com>
2021-07-17 11:26:15 -0300

    a

---
+++ README.md
@@ -0,0 +1,22 @@
+# OPA SUITE + MONGODB + PYTHON
+
+## A
+
+> Contador de atendimentos por canal
+
+- WhatsApp
+
+```js
+$[?(@.canal == "whatsapp")].length()
+$[?(@.canal == "messenger")].length()
+$[?(@.canal == "pabx")].length()
+```
+
+```js
+$[?(@.canal == "whatsapp")].length()
+$[?(@.id_atendente.oid == {#ID_ATENDENTE})].length()
+```
+
+```json
+
+```
---
+++ opaSuite.py
@@ -0,0 +1,63 @@
+import sys
+import pymongo
+from bson.json_util import dumps
+from bson import ObjectId
+
+params = sys.argv
+item = params[1]
+mydb = pymongo.MongoClient("mongodb://conectchat.com.br:27017/")["suite"]
+
+# EXEMPLO
+""" if item == "getAllUsuarios":
+    cursor = (mydb["usuarios"].find())
+    list_cur = list(cursor)
+    json_data = dumps(list_cur, indent = 3)
+    print(json_data) """
+
+if item == "getAllUsuarios":
+    cursor = (mydb["usuarios"].find())
+    list_cur = list(cursor)
+    json_data = dumps(list_cur)
+    print(json_data)
+
+if item == "getDepartamentos":
+    departamentos = (mydb["departamentos"].find({"status": "A"}))
+    list_cur = list(departamentos)
+    json_data = dumps(list_cur)
+    print(json_data)
+
+if item == "getFinalizados":
+    finalizados = (mydb["atendimentos"].find({"status": "F"}))
+    list_cur = list(finalizados)
+    json_data = dumps(list_cur)
+    print(json_data)
+
+if item == "getEmAtendimento":
+    emAtendimento = (mydb["atendimentos"].find({"status": "EA"}))
+    list_cur = list(emAtendimento)
+    json_data = dumps(list_cur)
+    print(json_data)
+
+if item == "getAguardandoAtendimento":
+    var = (mydb["atendimentos"].find({"status": "AG"}))
+    list_cur = list(var)
+    json_data = dumps(list_cur)
+    print(json_data)
+
+if item == "getEmEAguardandoAtendimento":
+    var = (mydb["atendimentos"].find({"status": "AG"}))
+    list_cur = list(var)
+    json_data = dumps(list_cur)
+    print(json_data)
+
+if item == "atendente":
+    print(mydb["atendimentos"].count_documents({"status":"EA","id_atendente": ObjectId(params[2])}))
+
+if item == "AGsetor":
+    print(mydb["atendimentos"].count_documents({"status": "AG","setor": ObjectId(params[2])}))
+
+if item == "setor":
+    print(mydb["atendimentos"].count_documents({"status": params[2],"setor": ObjectId(params[3])}))
+
+if item == "finalizadoHoje":
+    print(mydb.atendimentos.count_documents({"status":"F","id_atendente":ObjectId(params[2])}))
